55f0d55f45b8dafa97e009d4458aab34c586c491
irfangnr <zirfan733@gmail.com>
2024-09-30 17:36:10 +0700

    try seo nuxtjs

--- app.vue
+++ app.vue
@@ -4,3 +4,16 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'My App',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [{ innerHTML: 'console.log(\'Hello world\')' }]
+})
+</script>
